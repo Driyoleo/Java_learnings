03c9eb4ff9a642a3b1dc78dc8b9c178783ee602f
driyo <debashisr433@gmail.com>
2025-07-29 15:58:05 +0530

    init

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    // java is an object-oriented language so everthing is objected and to define an object we need class
+
+    public static void main(String[] a){
+
+        // public static void main (String[] a) is a way of saying this is the start of the code
+
+        System.out.print("hello world");
+
     }
 }
